cefce0902063b01ca769930fa4b9c1080dc968f2
Joel Saltzman <talk@joelsaltzman.com>
2016-01-10 17:22:33 -0800

    Initial Commit

---
+++ SwiftAsyncSocket/SwiftAsyncSocket.h
@@ -0,0 +1,19 @@
+//
+//  SwiftAsyncSocket.h
+//  SwiftAsyncSocket
+//
+//  Created by Joel Saltzman on 1/10/16.
+//  Copyright © 2016 Joel Saltzman. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for SwiftAsyncSocket.
+FOUNDATION_EXPORT double SwiftAsyncSocketVersionNumber;
+
+//! Project version string for SwiftAsyncSocket.
+FOUNDATION_EXPORT const unsigned char SwiftAsyncSocketVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftAsyncSocket/PublicHeader.h>
+
+
